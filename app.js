8f763a9cd361e243ba3fe3d35dc96f9bcacaf646
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2022-10-24 20:23:33 +0700

    updated

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(ProductRoute);
 
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 // const hostname = "192.168.0.123";
 app.listen(port, () => {
   console.log("Server up and runing....");
