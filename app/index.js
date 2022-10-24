b50b692244193febebb159119f639b50a8ccf3b7
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2022-10-24 20:49:16 +0700

    updated

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import ProductRoute from "./routes/ProductRoute.js";
+import ProductRoute from "../routes/ProductRoute.js";
 dotenv.config();
 
 const app = express();
